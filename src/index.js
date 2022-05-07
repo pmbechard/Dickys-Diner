8ce6370cd6f4664fd4b56efce7c658ba37e193d5
Peyton Bechard <pbechard@my.yorku.ca>
2022-05-07 18:58:58 +0800

    Adds all base files to working dir

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('test');
+import { renderHomePage } from "./home";
+import { renderMenuPage } from "./menu";
+import { renderContactPage } from "./contact";
+
+const initialDisplay = (renderFunc=renderHomePage) => {
+    renderFunc();
+};
+
+const home = document.getElementById('home');
+home.onclick = () => initialDisplay(renderHomePage);
+const menu = document.getElementById('menu');
+menu.onclick = () => initialDisplay(renderMenuPage);
+const contact = document.getElementById('contact');
+contact.onclick = () => initialDisplay(renderContactPage);
